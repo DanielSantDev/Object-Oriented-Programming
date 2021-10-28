46b4330344592b23f12a3ae90d05dde4599d0c4b
Daniel Santana <daniel10gja@gmail.com>
2021-10-28 13:07:38 -0300

    Interface

---
+++ src/pacote_interface/Classe_interface.java
@@ -0,0 +1,8 @@
+package pacote_interface;
+
+public interface Classe_interface {
+
+	public String identifica_fruta(String fruta);
+	public int quantidade_frutas(int f);
+	public double preco_kilo(double k);
+}
---
+++ src/pacote_interface/Implementa_Interface.java
@@ -0,0 +1,20 @@
+package pacote_interface;
+
+public class Implementa_Interface implements Classe_interface{
+	
+	protected String fruta_vermelha;
+	protected String fruta_citrica;
+	
+	public String identifica_fruta(String fruta) {
+		return fruta;
+	}
+	
+	public int quantidade_frutas(int f) {
+		return f;
+	}
+	
+	public double preco_kilo(double k) {
+		return k;
+	}
+
+}
